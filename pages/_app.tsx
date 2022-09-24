45a105306a96240b580f8d2c85066cafeb9bf3d0
Talla Giridhar <giridhar.talla2002@gmail.com>
2022-09-24 15:21:11 +0530

    project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 import type { AppProps } from 'next/app'
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <motion.div
+          initial="pageInitial"
+          animate="pageAnimate"
+          variants={{
+            pageInitial: {
+              opacity: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+            },
+          }}
+        >
+          <Component key={router.route} {...pageProps} />
+        </motion.div>
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
-
-export default MyApp
