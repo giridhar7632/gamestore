caffefdd2b50734cd006e00010e3178e5717eead
Talla Giridhar <giridhar.talla2002@gmail.com>
2022-10-07 14:42:03 +0530

    site verification

--- layout/Meta.tsx
+++ layout/Meta.tsx
@@ -27,7 +27,7 @@ const Meta: FC<MetaProps> = ({
     <meta name="keywords" content={keywords} />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <meta name="robots" content="index, follow" />
+    <meta name="google-site-verification" content="2MoK1TYNBlfHGllJGjNf-OjxhPujgb4FrznsWELiVkU" />
     <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="language" content="English" />
     <meta key="og_locale" property="og:locale" content="en_US" />
